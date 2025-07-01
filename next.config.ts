b989c79cb7f01371a59ffd3190cc515a15e3649f
Nilesh1409 <nileshtiwari7054@gmail.com>
2025-07-02 02:55:54 +0530

    switch to npm

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,8 +7,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  images: {
     domains: [
       "d3vp2rl7047vsp.cloudfront.net",
       "images.unsplash.com",
@@ -19,16 +14,10 @@ const nextConfig: NextConfig = {
       "lokeshshah.wordpress.com",
       "imgd.aeplcdn.com",
     ],
-    // alternatively, if you need pattern matching:
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "d3vp2rl7047vsp.cloudfront.net",
-    //     port: "",
-    //     pathname: "/bike_models/images/**",
-    //   },
-    // ],
   },
+  // Add this for static exports if needed
+  output: "export",
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig; // Change from export default
